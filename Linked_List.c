8b396245ff8c5b4cd1d2a229650dadbbd4b4a2fd
Slacky <98531038+Slacky300@users.noreply.github.com>
2022-08-11 20:31:55 +0530

    Update Linked_List.c

--- Linked_List.c
+++ Linked_List.c
@@ -2,59 +2,135 @@
 #include <stdlib.h>
 
 struct node{
+    
     int data;
     struct node *next;
-    
-}*head=NULL,*n1,*n2,*n3;
 
-void traverse(struct node *ptr){
-    int i=0;
-    while(ptr != NULL){
-        printf("Element at index %d = %d\n",i,ptr->data);
-        ptr = ptr->next;
-        i++;
+}*head = NULL,*p,*temp;
+
+struct node * addAtEnd(struct node *head,int value){
+
+
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->data=value;
+    p = head;
+    if(p == NULL){
+    
+       head  = temp;
+       head->next = NULL;
+    
+    }else{
+        while(p->next!=NULL){
+    
+            p = p->next;
+    
+        }
+
+        p->next = temp;
+        temp->next = NULL;
+    }
+    
+    
+    return head;
+
+}
+void display(){
+    
+    p = head;
+    if(head==NULL){   
+        printf("Linked list empty\n");
+    }else{
+        while(p!=NULL){
+            printf("%d ",p->data);
+            p = p->next;
+        }
     }
 }
-
-struct node * insertAtHead(struct node *head,int data){
-    struct node *ptr = (struct node *)malloc(sizeof(struct node));
-    ptr->next = head;
-    ptr->data = data;
-    return ptr;
+struct node * createList(struct node *head){
+    int i,limit,value;
+    printf("Enter the no. of node you want to add\n");
+    scanf("%d",&limit);
+    for(i = 0;i<limit;i++){
+        printf("Enter the value : ");
+        scanf("%d",&value);
+        head = addAtEnd(head,value);
+    }
     
+        return head;
 }
 
-struct node * insertInBetwn(struct node *head,int data,int index){
-    struct node *ptr = (struct node *)malloc(sizeof(struct node));
-    struct node *p = head;
-    int i = 0;
-    while(i != index-1){
+struct node * insertAfter(struct node *head,int value,int hold){
+   
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = hold;
+    p = head;
+    while(p->data!=value){
+        
         p = p->next;
-        i++;
+        if(p == NULL){
+            printf("Value : %d does not exists in the provided linked list\n",value);
+            return head;
+        }
     }
-    ptr->data = data;
-    ptr->next = p->next;
-    p->next = ptr;
+    temp->next = p->next;
+    p->next =  temp;
+    return head;
+    
+    
 }
 
+struct node * addAtBeg(struct node *head,int value){
+   
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = value;
+    temp->next = head;
+    head = temp;
+    return head;
+}
 
-void main(){
-    n1 =  (struct node *)malloc(sizeof(struct node));
-    n2 =  (struct node *)malloc(sizeof(struct node));
-    n3 =  (struct node *)malloc(sizeof(struct node));
-    n1->data = 45;
-    head = n1;
-    n1->next = n2;
-    n2->data = 54;
-    n2->next = n3;
-    n3->data = 65;
-    n3->next = NULL;
-    traverse(head);
+struct node * insertAtIndex(struct node *head,int location, int value){
+   
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = value;
+    p = head;
+    for(int i = 1;i<location;i++){
+       
+        if(p==NULL){
+            printf("Linked list exceeded\n");
+            return head;
+        }
+        
+        p = p->next;
+    }
+        temp->next=p->next;
+        p->next  = temp;
+        return head;
+}
+struct node * insertBefore(struct node *head,int value,int hold){
+   
+    temp = (struct node *)malloc(sizeof(struct node));
+    temp->data = hold;
+    p = head;
+    while(p->next->data!=value){
+        
+        p = p->next;
+        if(p == NULL){
+            printf("Value : %d does not exists in the provided linked list\n",value);
+            return head;
+        }
+    }
     
-    head = insertAtHead(head,48);
-    printf("New linked list\n");
-    traverse(head);
-    insertInBetwn(head,85,2);
-    printf("New linked list\n");
-    traverse(head);
+    temp->next = p->next;
+    p->next =  temp;
+    return head;
+    
+    
+}
+void main(){
+    head = createList(head);
+    head = insertAfter(head,52,98);
+    head = addAtBeg(head,50);
+    head = insertAtIndex(head,3,89);
+    head = insertBefore(head,52,98);
+    display();
 }
