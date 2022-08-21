03efcbbd3c4ac3ad7c7d8d711154cff3bc1047e7
Slacky <98531038+Slacky300@users.noreply.github.com>
2022-08-21 15:17:25 +0530

    Update Linked_List.c

--- Linked_List.c
+++ Linked_List.c
@@ -6,7 +6,7 @@ struct node{
     int data;
     struct node *next;
 
-}*head = NULL,*p,*temp;
+}*head = NULL,*p,*temp,*head2 = NULL;
 
 
 
@@ -37,7 +37,7 @@ struct node * addAtEnd(struct node *head,int value){
     return head;
 
 }
-void display(){
+void display(struct node *head){
     
     p = head;
     if(head==NULL){   
@@ -196,13 +196,76 @@ void deleteSpecific(struct node *head,int value){
     
 }
 
+struct node *split(struct node *head,int value){
+    temp = head;
+    while(temp!=NULL && temp->data!=value){
+        temp = temp->next;
+    }
+    
+    head2 = temp->next;
+    temp->next = NULL;
+    return head2;
+}
+void merge(struct node *head,struct node *head2){
+    temp = head;
+    if(head2==NULL){
+        printf("Merge operation can't be performed\n");
+        return;
+    }
+    while(temp!=NULL&&temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=head2;
+    head2 =NULL;
+}
+
+
+struct node *reverseList(struct node *head){
+    struct node *prev,*nx,*curr;
+    curr =  head;
+    prev = NULL;
+    while(curr!= NULL){
+        nx = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nx;
+    }
+    head = prev;
+    return head;
+}
+
+void sort(struct node *head){
+   int going = 0;
+   temp = NULL;
+   if(head==NULL){
+       return;
+   }
+   do{
+       going = 0;
+       p = head;
+       while(p->next!=temp){
+           if(p->data>p->next->data){
+               swap(p,p->next);
+               going = 1;
+           }
+           p=p->next;
+       }
+       temp = p;
+   }while(going);
+}
+void swap(struct node *a,struct node *b){
+    int temp = a->data;
+    a->data = b->data;
+    b->data = temp;
+}
+
 void main(){
     
     
     int choice,choice2,choice3;
     do{
         printf("Enter the operation you want to perform\n");
-        printf("1.CreateList\n2.Insert\n3.Count\n4.Delete\n5.Display\n6.Exit\n");
+        printf("1.CreateList\n2.Insert\n3.Count\n4.Delete\n5.Display\n6.Split\n7.Merge\n8.Reverse\n9.Sort\n10.Exit\n");
         scanf("%d",&choice);
         if(choice == 1){
             head = createList(head);
@@ -225,29 +288,29 @@ void main(){
                 scanf("%d",&hold);
                 head = insertAtIndex(head,index,hold);
             }else if(choice2==3){
-                printf("Enter the index and value respectively\n");
-                scanf("%d %d",&index,&hold);
-                head = insertAtIndex(head,index,hold);
+                printf("Enter the value to add at last\n");
+                scanf("%d",&hold);
+                head = addAtEnd(head,hold);
                 printf("Linked list: \n");
-                display();
+                display(head);
             }else if(choice2==4){
-                display();
+                display(head);
                 printf("Enter the value\n");
                 scanf("%d",&hold);
                 printf("Enter the element\n");
                 scanf("%d",&index);
                 head = insertBefore(head,index,hold);
                 printf("Linked list: \n");
-                display();
+                display(head);
             }else if(choice2 == 5){
-                display();
+                display(head);
                 printf("Enter the value\n");
                 scanf("%d",&hold);
                 printf("Enter the element\n");
                 scanf("%d",&index);
                 head = insertAfter(head,index,hold);
                 printf("Linked list: \n");
-                display();
+                display(head);
                 
             }else if(choice2==6){
                 break;
@@ -266,17 +329,17 @@ void main(){
                 if(choice3==1){
                     head = removeHead(head);
                     printf("Linked List:\n");
-                    display();
+                    display(head);
                 }else if(choice3==2){
                     printf("Enter the value\n");
                     scanf("%d",&hold);
                     deleteSpecific(head,hold);
                     printf("Linked List:\n");
-                    display();
+                    display(head);
                 }else if(choice3==3){
                     removeLast();
                     printf("Linked List:\n");
-                    display();
+                    display(head);
                 }else if(choice3==4){
                     break;
                 }else{
@@ -286,13 +349,44 @@ void main(){
             }while(choice3!=4);
         }else if(choice==5){
             printf("Linked List:\n");
-            display();
+            display(head);
         }else if(choice==6){
-            break;
+            int hold;
+            printf("Enter the value till you want the linked list\n");
+            scanf("%d",&hold);
+            head2 = split(head,hold);
+            printf("New Linked List after splitting\n");
+            printf("Linked list 1: ");
+            display(head);
+            printf("Linked list 2: ");
+            display(head2);
+            
+            
+        }else if(choice==7){
+            merge(head,head2);
+            printf("Merged Succesfully\nCurrent Linked list:\n");
+            display(head);
+        }else if(choice==8){
+                head = reverseList(head);
+                printf("Reverse list:\n");
+                display(head);
+           
+        }else if(choice==9){
+            
+                sort(head);
+                printf("Sorted List:\n");
+                display(head);
+           
+        }else if(choice==10){
+            
+                    break;
+           
         }else{
-            printf("Invalid input\n");
+             printf("Invalid Input\n");
         }
-    }while(choice!=6);
+    }while(choice!=10);
         printf("End of linked list operations\nFinal Linked List: ");
-        display();
+        display(head);
+        
+        
 }
